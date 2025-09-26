bd1b0bd1fe6ff16b0e9c6d8bf3805e4f4205fe50
Jarif <xjarifx@gmail.com>
2025-09-26 23:11:58 +0600

    feat: add follow/unfollow routes and user followers/following retrieval to the user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,10 @@ import {
   handleUserProfileGet,
   handleUserProfileUpdate,
   handleChangePassword,
+  handleGetFollowing,
+  handleGetFollowers,
+  handleUnfollowUser,
+  handleFollowUser,
 } from "../controllers/user-controller.js";
 import { authenticateUserToken } from "../middlewares/auth-middleware.js";
 
@@ -17,6 +21,14 @@ router.post("/users/login", handleUserLogin);
 // Profile routes (protected)
 router.get("/users/profile", authenticateUserToken, handleUserProfileGet);
 router.put("/users/profile", authenticateUserToken, handleUserProfileUpdate);
+
+// Change password route (protected)
 router.put("/users/password", authenticateUserToken, handleChangePassword);
+
+// Follow/Unfollow routes (protected)
+router.post("/users/:targetUsername/follow", authenticateUserToken, handleFollowUser);
+router.delete("/users/:targetUsername/unfollow", authenticateUserToken, handleUnfollowUser);
+router.get("/users/followers", authenticateUserToken, handleGetFollowers);
+router.get("/users/following", authenticateUserToken, handleGetFollowing);
 
 export default router;
